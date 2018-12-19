4c74fe8b387172e065f788943d73598184851978
Ralakus <12schneide@gmail.com>
2018-12-19 17:34:03 -0500

    Fixed stuff, it was great

--- Assets/spud.gd
+++ Assets/spud.gd
@@ -2,12 +2,16 @@ extends KinematicBody2D
 
 var direction = Vector2()
 var velocity  = Vector2()
+var previous_velocity = Vector2()
 var speed     = 25000.0
 var timer = 0
+var stop  = false
+var stop_timer = 0
 
 
 
 func _ready():
+	
 	pass
 	
 func go(coordinate, direction):
@@ -21,8 +25,17 @@ func _process(delta):
 	if(timer>10):
 		get_parent().remove_child(self)
 	
-	velocity = delta * direction.normalized() * speed
+	if(stop == false):
+		velocity = delta * direction.normalized() * speed
+	else:
+		stop_timer+=delta;
+		if(stop_timer > 0.05):
+			get_parent().remove_child(self)
+	previous_velocity = velocity;
 	velocity = move_and_slide(velocity)
-	
-	if(velocity < Vector2(1, 1) and timer >1):
+	if(velocity != previous_velocity):
+		stop = true;
+		velocity = Vector2(0,0);
+		stop_timer = 0;
+	if(timer >1):
 		get_parent().remove_child(self)
